12222704015162321964e42584e468baaa6aa0b5
d-dhathrichandra <ddhathri.chandra.ece21@itbhu.ac.in>
2024-02-03 19:33:36 +0530

    add

--- chandu-portfolio/src/Components/Footer1.js
+++ chandu-portfolio/src/Components/Footer1.js
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <div className="social-links">
-          <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/d-dhathrichandra" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/d-dhathrichandra" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>
           </a>
           {/* Add more social links as needed */}
--- chandu-portfolio/src/Components/Projects1.js
+++ chandu-portfolio/src/Components/Projects1.js
@@ -8,21 +8,21 @@ const Projects = () => {
         <div className="projects-content">
           <h2>Projects</h2>
           <div className="projects-item">
-            <img src="https://www.mdpi.com/energies/energies-12-02692/article_deploy/html/images/energies-12-02692-g005.png" alt="Project 1" />
+            <img src="https://i.ytimg.com/vi/jeKKJ-T1D_Q/maxresdefault.jpg" alt="Project 1" />
             <div className="item-details">
               <h3>Automatic Street Lights using 555 timer</h3>
               <p> Designed and implemented an innovative embedded systems project focused on energy-saving techniques for street lights.</p>
               <p>Utilized Light Dependent Resistors (LDR) as sensors to detect darkness, triggering an automatic switch-on mechanism for the lights. Additionally, incorporated a circuit based on a 555 Timer to turn off the street lights when ambient daylight is detected from an alternate light source.</p>
-              <a href="https://www.linkedin.com/in/dnrk-charan/details/projects/">View Project</a>
+              <a href="https://www.linkedin.com/in/d-dhathrichandra/details/projects/">View Project</a>
             </div>
           </div>
           <div className="projects-item">
-            <img src="https://cdn.techjockey.com/blog/wp-content/uploads/2021/09/Student-Information-Management-System-_Banner.jpg" alt="Project 2" />
+            <img src="https://projectabstracts.com/wp-content/uploads/2018/08/pg-20827-student-database-management.jpg" alt="Project 2" />
             <div className="item-details">
-              <h3>Student-Faculty Database Management</h3>
-              <p>Created Database containing student and faculty information using MySQL and Python. Developed an algorithm to map the interest of students with the corresponding faculty specialization.</p>
-                <p>The project allowed students to find a suitable project supervisor.</p>
-              <a href="https://www.linkedin.com/in/dnrk-charan/details/projects/">View Project</a>
+              <h3>Student-Department Database Management</h3>
+              <p> Created Database containing Student details and Department information using MySQL and Python.</p>
+                <p>Developed an algorithm to map the Details of students with the corresponding Department or specialization.</p>
+              <a href="https://www.linkedin.com/in/d-dhathrichandra/details/projects/">View Project</a>
             </div>
           </div>
           {/* Add more projects items as needed */}
